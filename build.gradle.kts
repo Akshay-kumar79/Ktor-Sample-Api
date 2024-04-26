7c41e1127ff9c6d06cb64715b6ae65d0f74d026a
Akshay kumar shaw <akshay.8459361606@gmail.com>
2024-04-26 20:35:00 +0530

    Setup for Google cloud deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -6,6 +8,8 @@ plugins {
     kotlin("jvm") version "1.9.23"
     id("io.ktor.plugin") version "2.3.10"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.google.cloud.tools.appengine") version "2.4.2"
 }
 
 group = "akshaw.com"
@@ -17,6 +21,10 @@ application {
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+kotlin {
+    jvmToolchain(11)
 }
 
 repositories {
@@ -39,13 +47,31 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
-tasks.register<Jar>("fatJar") {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    manifest {
-        attributes("Main-Class" to mainClassName)
+//tasks.register<Jar>("fatJar") {
+//    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+//    manifest {
+//        attributes("Main-Class" to mainClassName)
+//    }
+//    from(configurations.runtimeClasspath.get().map {
+//        if (it.isDirectory) it else zipTree(it)
+//    })
+//    with(tasks.jar.get())
+//}
+
+configure<AppEngineAppYamlExtension> {
+    stage {
+        setArtifact("build/libs/${project.name}-all.jar")
     }
-    from(configurations.runtimeClasspath.get().map {
-        if (it.isDirectory) it else zipTree(it)
-    })
-    with(tasks.jar.get())
+    deploy {
+        version = "GCLOUD_CONFIG"
+        projectId = "GCLOUD_CONFIG"
+    }
+}
+
+tasks {
+    shadowJar {
+        manifest {
+            attributes["Main-Class"] = mainClassName
+        }
+    }
 }
